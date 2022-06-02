710adb997296f83fe1ea6ada5fe2e05b34690118
Seth <seth-xdam@users.noreply.github.com>
2022-06-01 20:38:41 -0400

    Add division confirmation query

--- schema/conversion-queries.sql
+++ schema/conversion-queries.sql
@@ -1,9 +1,9 @@
-# Add some fields that will be used for report views
+-- Add some fields that will be used for report views
 alter table Entries add EVENT_TYPE char;
 alter table Entries add D_ABBV varchar(4) AFTER AGE_GROUP;
 alter table Entries add division INT;
 
-# Connect entry records to pre-built divisions
+-- Connect entry records to pre-built divisions
 update Entries E
   join so_divisions D on E.EVENT like concat(D.level,'%') and E.AGE_GROUP = D.age_group
   set E.division = D.division_num;
@@ -16,9 +16,16 @@ update Entries E
 update Entries E
   join so_divisions D on D.age_group = '  8+   ' AND E.AGE_GROUP = '  8- 21' AND D.level = 'Level 2/3' and E.EVENT LIKE 'Level 2 / 3%'
   set E.division = D.division_num;
+
+-- Confirm matching divisions
+SELECT concat(E.EVENT,' ', E.AGE_GROUP), concat(D.level,' ',D.age_group)
+FROM Entries AS E
+JOIN so_divisions AS D ON D.division_num = E.division
+ORDER BY E.EVENT;
+
 UPDATE Entries SET D_ABBV = upper(substring(DELEGATION,6,4));
 
-# Check for duplicate delegations or "bad" abbreviations. The following UPDATE queries correct ones found in previous years.
+-- Check for duplicate delegations or "bad" abbreviations. The following UPDATE queries correct ones found in previous years.
 select DELEGATION, D_ABBV, count(*) from Entries GROUP BY DELEGATION order by D_ABBV ;
 
 UPDATE Entries SET D_ABBV = 'MCS' Where DELEGATION LIKE '% County - South';
@@ -34,10 +41,10 @@ UPDATE Entries SET D_ABBV = 'MAST' Where D_ABBV = 'MARS';
 UPDATE Entries SET D_ABBV = 'ROD ' Where D_ABBV = 'RIPL';
 UPDATE Entries SET D_ABBV = 'STJO' Where D_ABBV = 'ST. ';
 
-# Double check
+-- Double check
 select DELEGATION, D_ABBV, count(*) from Entries GROUP BY DELEGATION order by D_ABBV ;
 
-# Populate the EVENT_SHOR and EVENT_TYPE columns
+-- Populate the EVENT_SHOR and EVENT_TYPE columns
 UPDATE Entries SET EVENT_SHOR = '50'   WHERE EVENT LIKE '% 50 M %';
 UPDATE Entries SET EVENT_SHOR = '100'  WHERE EVENT LIKE '% 100 M %';
 UPDATE Entries SET EVENT_SHOR = '200'  WHERE EVENT LIKE '% 200 M %';
