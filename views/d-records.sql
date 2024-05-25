d37e35ea4e12e2fe6a88dbd1a2e06e047a684688
Seth Foss <thelizardreborn@live.com>
2024-05-25 13:49:39 -0400

    Fix column names for event view

--- views/d-records.sql
+++ views/d-records.sql
@@ -1,19 +1,19 @@
 CREATE OR REPLACE VIEW `entries` AS
 SELECT
    'd' AS record_type,
-   SUBSTRING_INDEX(ENTRANT_NA, ',', 1) AS 'last_name',
+   SUBSTRING_INDEX(`NAME`, ',', 1) AS 'last_name',
    IF (
-      LOCATE(' ', TRIM(SUBSTRING_INDEX(ENTRANT_NA, ',', -1))) = 0,
-      TRIM(SUBSTRING_INDEX(ENTRANT_NA, ',', -1)), 
+      LOCATE(' ', TRIM(SUBSTRING_INDEX(`NAME`, ',', -1))) = 0,
+      TRIM(SUBSTRING_INDEX(`NAME`, ',', -1)), 
       LEFT(
-         TRIM(SUBSTRING_INDEX(ENTRANT_NA, ',', -1)), 
-         LENGTH(TRIM(SUBSTRING_INDEX(ENTRANT_NA, ',', -1))) - LENGTH(SUBSTRING_INDEX(TRIM(SUBSTRING_INDEX(ENTRANT_NA, ',', -1)),' ',-1))-1
+         TRIM(SUBSTRING_INDEX(`NAME`, ',', -1)), 
+         LENGTH(TRIM(SUBSTRING_INDEX(`NAME`, ',', -1))) - LENGTH(SUBSTRING_INDEX(TRIM(SUBSTRING_INDEX(`NAME`, ',', -1)),' ',-1))-1
       )
    ) AS 'first_name',
    IF (
-      LOCATE(' ', TRIM(SUBSTRING_INDEX(ENTRANT_NA, ',', -1))) = 0,
+      LOCATE(' ', TRIM(SUBSTRING_INDEX(`NAME`, ',', -1))) = 0,
       '',
-      LEFT(SUBSTRING_INDEX(TRIM(SUBSTRING_INDEX(ENTRANT_NA, ',', -1)), ' ', -1),1)
+      LEFT(SUBSTRING_INDEX(TRIM(SUBSTRING_INDEX(`NAME`, ',', -1)), ' ', -1),1)
    ) AS 'initial',
    GENDER AS 'gender',
    DOB AS 'birth_date',
@@ -25,7 +25,7 @@ SELECT
    QUAL_S AS 'entry_mark',
    'M' AS 'event_measure', -- M for Metric
    division AS 'division',
-   `BIB__` AS 'bib',
+   `BIB` AS 'bib',
    '' AS 'finish_place',
    '' AS 'declaration',
    '' AS 'notes',
